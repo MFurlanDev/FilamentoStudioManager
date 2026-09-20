a3219ae5b3af83c73e96f7b771bce1b163273f57
Mario Furlan <mfurlandev@gmail.com>
2026-09-20 23:10:37 +0200

    Menú principal diseñado

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,24 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("FilamentoStudio Manager");
-        System.out.println("v0,0,1");
+        System.out.println("=============================");
+        System.out.println("   FILAMENTOSTUDIO MANAGER");
+        System.out.println("          v0.0.1");
+        System.out.println("=============================");
+        System.out.println("1. Registrar pedido");
+        System.out.println("2. Ver pedidos");
+        System.out.println("3. Calcular presupuesto");
+        System.out.println("4. Registrar material");
+        System.out.println("5. Ver inventario");
+        System.out.println("6. Registrar gasto");
+        System.out.println("7. Ver resumen económico");
+        System.out.println("0. Salir");
+        System.out.println("-----------------------------");
+
+
+        System.out.print("Selecciona una opción (introduce sólo el número): ");
+        Scanner scanner = new Scanner(System.in);
+        int opcionElegida = scanner.nextInt();
     }
 }
